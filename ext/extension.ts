bfffd9e6e37d357e841412689f570f1d9067dcc7
Shota Ikehara <freedomikeppp@gmail.com>
2021-08-28 01:27:07 +0900

    VS Codeの拡張機能でVueを利用しHello World表示

---
+++ ext/extension.ts
@@ -0,0 +1,50 @@
+import * as vscode from "vscode";
+import * as path from "path";
+
+export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand(
+    "extension-practice.vue-try",
+    () => {
+      const panel = vscode.window.createWebviewPanel(
+        `vue-practice`,
+        `Vue Practice`,
+        vscode.ViewColumn.One,
+        {
+          enableScripts: true,
+          // VS Codeのタブを切り替えるとTopページに強制的に戻るのを防ぐ。
+          // ただし、メモリ消費が大きいとのこと。
+          // https://code.visualstudio.com/api/extension-guides/webview#retaincontextwhenhidden
+          retainContextWhenHidden: true,
+        }
+      );
+      panel.webview.html = getWebviewContent(context, panel.webview);
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+function getWebviewContent(
+  context: vscode.ExtensionContext,
+  webview: vscode.Webview
+): string {
+  const scriptUri = webview.asWebviewUri(
+    vscode.Uri.file(
+      path.join(context.extensionPath, "src", "dist", "main.js")
+    )
+  );
+  return `<!DOCTYPE html>
+          <html lang="ja">
+          <head>
+              <meta charset="UTF-8">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <title>Hello</title>
+          </head>
+          <body>
+              <div id="entry"></div>
+              <script src=${scriptUri}></script>
+          </body>
+          </html>`;
+}
+
+export function deactivate() {}
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+//@ts-ignore
+import Top from "./Top";
+
+import Vue from "vue";
+import VueRouter, { RouterOptions } from "vue-router";
+
+const routerOption: RouterOptions = {
+  routes: [
+    { path: "/", component: Top }
+  ],
+};
+const router = new VueRouter(routerOption);
+Vue.use(VueRouter);
+router.push("/");
+new Vue({
+  el: `#entry`,
+  template: `<router-view></router-view>`,
+  router,
+});
